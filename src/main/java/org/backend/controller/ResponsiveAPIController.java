cac266d1056bcbb57657911b238632c1f081671f
Jae-Young98 <ygy0102@gmail.com>
2025-06-28 16:29:51 +0900

    feat: WebFlux 기반 반응형 메시지 스트림 기능 추가

---
+++ src/main/java/org/backend/controller/ResponsiveAPIController.java
@@ -0,0 +1,22 @@
+package org.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.backend.dto.BaseResponse;
+import org.backend.service.ResponsiveAPIService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequiredArgsConstructor
+public class ResponsiveAPIController {
+
+    private final ResponsiveAPIService responsiveAPIService;
+
+    @GetMapping(value = "/messages/greeting", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<BaseResponse<String>> getHelloWorldMessage() {
+        return responsiveAPIService.getHelloWorldMessage()
+                .map(BaseResponse::new);
+    }
+}
