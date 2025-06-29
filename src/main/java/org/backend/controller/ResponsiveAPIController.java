584304c5b235e66ff051a35685a9e32a1f5b7f51
Jae-Young98 <ygy0102@gmail.com>
2025-06-29 11:02:49 +0900

    refactor: 반응형 메시지 스트림 기능 응답 수정

--- src/main/java/org/backend/controller/ResponsiveAPIController.java
+++ src/main/java/org/backend/controller/ResponsiveAPIController.java
@@ -2,6 +2,7 @@ package org.backend.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.backend.dto.BaseResponse;
+import org.backend.dto.response.SseResponse;
 import org.backend.service.ResponsiveAPIService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ public class ResponsiveAPIController {
     private final ResponsiveAPIService responsiveAPIService;
 
     @GetMapping(value = "/messages/greeting", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<BaseResponse<String>> getHelloWorldMessage() {
+    public Flux<BaseResponse<SseResponse>> getHelloWorldMessage() {
         return responsiveAPIService.getHelloWorldMessage()
                 .map(BaseResponse::new);
     }
